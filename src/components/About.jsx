aa6e9364c6714226d58b0484167785caa63ebf8e
Koushik Roy <jc.koushikroy@gmail.com>
2024-09-23 22:47:21 +0530

    about styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import AboutPageImg from "../assets/AboutPageImg.png";
+import "../css/About.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 const aboutPage = () => {
-  return <div>aboutPage</div>;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <div className="about-image">
+            <img src={AboutPageImg} alt="AboutPageImg" />
+          </div>
+        </Col>
+        <Col> this is 2nd container</Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default aboutPage;
