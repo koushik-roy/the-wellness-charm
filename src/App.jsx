c71c0ca426687f35b525bb8334f1337f2df11614
Koushik Roy <jc.koushikroy@gmail.com>
2024-09-23 18:57:39 +0530

    modified title and favicon for application

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Banner from "./components/Banner";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Banner/>
+      <Banner />
       <NavBar />
       <Home />
-
-
     </>
   );
 }
