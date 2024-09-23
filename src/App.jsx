0a6f69bced4000a3f04541115d996574cc43ba33
Koushik Roy <jc.koushikroy@gmail.com>
2024-09-23 21:58:15 +0530

    added router in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import "./App.css";
+import {
+  Routes,
+  BrowserRouter,
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Banner from "./components/Banner";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Banner />
       <NavBar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
